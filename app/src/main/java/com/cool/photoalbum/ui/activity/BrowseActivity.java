64e10c5b4f9474da988b74f40f424d75fb31508b
dongyumao@tiange.com <dongyumao@tiange.com>
2020-10-10 18:55:00 +0800

    1. 添加分享 和 喜欢，未完成

--- app/src/main/java/com/cool/photoalbum/ui/activity/BrowseActivity.java
+++ app/src/main/java/com/cool/photoalbum/ui/activity/BrowseActivity.java
@@ -1,7 +1,16 @@
 package com.cool.photoalbum.ui.activity;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -16,12 +25,23 @@ import com.cool.photoalbum.model.domain.PhotoList;
 import com.cool.photoalbum.ui.adapter.BrowseAdapter;
 import com.cool.photoalbum.utils.Constants;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class BrowseActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private BrowseAdapter mAdapter;
+    private View mIconBack;
+    private View mIconLike;
+    private View mIconShare;
+    private View mIconDownload;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,10 +52,61 @@ public class BrowseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_browse);
 
         initView();
+
+        initEvent();
+    }
+
+    private void initEvent() {
+        mIconBack = findViewById(R.id.browse_icon_back);
+        mIconLike = findViewById(R.id.browse_icon_like_img);
+        mIconShare = findViewById(R.id.browse_icon_share);
+        mIconDownload = findViewById(R.id.browse_icon_download);
+
+        mIconBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mIconLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIconLike.setBackgroundResource(R.mipmap.browse_like_selecte);
+            }
+        });
+
+        mIconShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mIconDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int postion = ((LinearLayoutManager)mRecyclerView.getLayoutManager()).findFirstCompletelyVisibleItemPosition();
+                PhotoList.FeedsBean bean = mAdapter.getData().get(postion);
+
+                // 调用系统分享
+
+                /** * 分享图片 */
+                Bitmap bgimg0 = getImageFromAssetsFile(bean.getImage_large());
+                Intent share_intent = new Intent();
+                share_intent.setAction(Intent.ACTION_SEND);//设置分享行为
+                share_intent.setType("*/*");  //设置分享内容的类型
+                share_intent.putExtra(Intent.EXTRA_STREAM, saveBitmap(bgimg0,"img"));
+                //创建分享的Dialog
+                share_intent = Intent.createChooser(share_intent, "dialogTitle");
+                startActivity(share_intent);
+            }
+        });
+
     }
 
 
-    protected void initView() {
+    private void initView() {
 
         mRecyclerView= findViewById(R.id.browse_recycler_view);
 
@@ -53,5 +124,40 @@ public class BrowseActivity extends AppCompatActivity {
         Intent intent = getIntent();
         ArrayList<PhotoList.FeedsBean> list1 = intent.getParcelableArrayListExtra(Constants.KEY_FEED_BEAN_LIST);
         mAdapter.setList(list1);
+    }
+
+    /** * 从Assets中读取图片 */
+    private Bitmap getImageFromAssetsFile(String fileName){
+        Bitmap image = null;
+        AssetManager am = getResources().getAssets();
+        try {
+            InputStream is=am.open(fileName);
+            image=BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();    }
+        return image;
+    }
+
+    /** * 将图片存到本地 */
+    private static Uri saveBitmap(Bitmap bm, String picName) {
+        try {
+            String dir= Environment.getExternalStorageDirectory().getAbsolutePath()+"/renji/"+picName+".jpg";
+            File f = new File(dir);
+            if (!f.exists()) {
+                f.getParentFile().mkdirs();
+                f.createNewFile();
+            }
+            FileOutputStream out = new FileOutputStream(f);
+            bm.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.flush();
+            out.close();
+            Uri uri = Uri.fromFile(f);
+            return uri;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();    }
+        return null;
     }
 }

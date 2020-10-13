97f5181c932be837984bc96a731e7af9acb4e228
dongyumao@tiange.com <dongyumao@tiange.com>
2020-10-13 18:41:31 +0800

    1. 添加Glide工具类，清除缓存功能

--- app/src/main/java/com/cool/photoalbum/ui/fragment/SetupFragment.java
+++ app/src/main/java/com/cool/photoalbum/ui/fragment/SetupFragment.java
@@ -6,11 +6,13 @@ import android.widget.TextView;
 import com.cool.photoalbum.R;
 import com.cool.photoalbum.base.BaseApplication;
 import com.cool.photoalbum.base.BaseFragment;
+import com.cool.photoalbum.utils.GlideCacheUtil;
 import com.cool.photoalbum.utils.PushActivityUtil;
 
 public class SetupFragment extends BaseFragment {
     private TextView mTitle;
     private View mDownRow;
+    private View mClearCache;
 
     @Override
     protected int getRootViewResId() {
@@ -24,14 +26,24 @@ public class SetupFragment extends BaseFragment {
         mTitle.setText(BaseApplication.getAppContext().getString(R.string.set_up));
 
         mDownRow = rootView.findViewById(R.id.set_up_look_download);
+        mClearCache = rootView.findViewById(R.id.set_up_clear_cache);
     }
 
     @Override
     protected void initListener() {
+        // 跳转到下载界面
         mDownRow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PushActivityUtil.toDownloadActivity(getContext());
+            }
+        });
+
+        // 清除缓存
+        mClearCache.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GlideCacheUtil.getOurInstance().clearImageAllCache(getContext());
             }
         });
     }
